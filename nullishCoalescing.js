59ecbf5d9975f3c0aea4b335a380e6525aec494b
MuhammadZikri <zikrim577@gmail.com>
2023-01-16 14:48:37 +0700

    Nullish Coalescing - Javascript

---
+++ nullishCoalescing.js
@@ -0,0 +1,15 @@
+// membuat variabel name
+let name = "";
+let myname = name ?? "Aya";
+
+// membuat variabel angka
+const angka = "";
+const number = angka || 78;
+
+// membuat variabel huruf 
+const huruf = "";
+const karakter = huruf ?? (umur > 18 ? "Remaja" : "Bocil");
+
+// membuat variabel parameter
+let parameter;
+let meter = parameter ?? "Undefined";
